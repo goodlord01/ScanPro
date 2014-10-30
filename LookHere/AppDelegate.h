07c5c0009a86a2b2849572f12f0c40eb6dab62c8
Hu Min <sanguineok@gmail.com>
2014-10-30 15:22:28 +0800

    add files

---
+++ LookHere/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LookHere
+//
+//  Created by min on 10/30/14.
+//  Copyright (c) 2014 codeall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LookHere/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LookHere
+//
+//  Created by min on 10/30/14.
+//  Copyright (c) 2014 codeall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
